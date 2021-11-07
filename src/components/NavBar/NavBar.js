835f3aab3239b07305bd89664d2b2cb7ed6440ec
ignaciocorcos <ignaciocorcos@gmail.com>
2021-11-07 18:51:38 -0300

    NavBar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,22 @@
+export const NavBar = () => {
+
+    return(
+        <div className="header">
+            <h1> Logo </h1>
+            <nav>
+                <ul>
+                    <li>NEW</li>
+                    <li>Soldadura</li>
+                    <li>Rectificadores</li>
+                    <li>Ofertas</li>
+                    <li>Usados</li>
+                </ul>
+            </nav>
+    
+        </div>
+        ) 
+        
+        
+}
+
+//export default NavBar
